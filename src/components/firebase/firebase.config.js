14a6a6aedbcbc2905d219ca92798fcacdc500a28
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-05-19 18:30:28 +0600

    firebase key hide

--- src/components/firebase/firebase.config.js
+++ src/components/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmeAKuk-M14bVdMlBF-v3zHApm0RS_lMI",
-  authDomain: "sports-special-ca4dd.firebaseapp.com",
-  projectId: "sports-special-ca4dd",
-  storageBucket: "sports-special-ca4dd.appspot.com",
-  messagingSenderId: "276960266444",
-  appId: "1:276960266444:web:cb26609f14ce1721a21a69"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
