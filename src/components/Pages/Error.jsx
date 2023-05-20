a5298738ab5dc2918bec53b2621c1f4cd4769e32
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-05-20 16:58:53 +0600

    error page finish

--- src/components/Pages/Error.jsx
+++ src/components/Pages/Error.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import errorImg from '../../assets/Images/error.png'
 
 const Error = () => {
     return (
         <div className='flex items-center justify-center min-h-screen w-full'>
-            <div className='w-full text-center'>
-                <img src="" alt="" />
-                <h1 className='text-2xl text-gray-500 text-center font-bold uppercase'>not found data</h1>
+            <div className='w-full flex flex-col items-center text-center'>
+                <img className='w-80 mb-10' src={errorImg} alt="" />
+                <h1 className='text-2xl text-gray-500 text-center font-bold uppercase'>not found page</h1>
                 <Link to='/'>
                 <button className='btn mt-7 text-center bg-orange-600 border-none'>BACK TO HOME</button>
                 </Link>
